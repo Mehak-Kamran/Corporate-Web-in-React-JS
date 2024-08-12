b10ef701c9ab0ef7c015f6ecdb46da51b28472c4
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-08-12 23:09:10 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Banner from "./components/Banner.jsx"
+
+function App() {
+  
+
+  return (
+    <>
+    <Banner/>
+       
+        
+      
+       
+      
+    </>
+  )
+}
+
+export default App
